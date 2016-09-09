9515292f0841c80158d37f30a28f137849af84ac
v-xucxia <v-xucxia@PC-WINFFEE>
2016-09-09 10:16:16 +0800

    Add project files.

---
+++ ResumeSample/js/newPage.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict"
+
+
+    var roaming = Windows.Storage.ApplicationData.current.roamingSettings;
+
+    Windows.UI.WebUI.WebUIApplication.addEventListener("suspending", function (args) {
+        roaming["currentUri"] = window.location.href;
+        //save the other informations of the page
+    });
+
+    window.onload = function () {
+        document.getElementById("myBtn").onclick = function () {
+            window.location.href = "ms-appx:///index.html";
+        }
+    }
+    
+
+})();
